9750ccb85d47e3bff6172e352d5c84dc73c2a4d7
DmZabelin <contact@dmz.name>
2022-12-23 19:33:13 +0200

    Created pizza components and categories, list renderingÐ

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import './scss/app.scss';
-import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+import pizzas from './pizzas.json';
 
 function App() {
   return (
-    <div class='wrapper'>
+    <div className='wrapper'>
       <Header />
-      <div class='content'>
-        <div class='container'>
-          <div class='content__top'>
+      <div className='content'>
+        <div className='container'>
+          <div className='content__top'>
             <Categories />
             <Sort />
           </div>
-          <h2 class='content__title'>Все пиццы</h2>
-          <div class='content__items'>
-              <PizzaBlock title={'Мексиканская'} price={'3000'}/>
+          <h2 className='content__title'>Все пиццы</h2>
+          <div className='content__items'>
+            {pizzas.map((data, index) => (
+              <PizzaBlock key={index} data={data} />
+            ))}
           </div>
         </div>
       </div>
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,26 @@
+import React from 'react';
+
+const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
 function Categories() {
-    return (
-        <div className='categories'>
-            <ul>
-                <li className='active'>Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
-            </ul>
-        </div>
-    )
+  const [isCategoryActive, setIsCategoryActive] = React.useState(0);
+
+  return (
+    <div className='categories'>
+      <ul>
+        {categories.map((item, index) => {
+          return (
+            <li
+              onClick={() => setIsCategoryActive(index)}
+              key={index}
+              className={isCategoryActive === index ? 'active' : ''}>
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Categories;
